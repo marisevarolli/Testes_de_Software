4863050c651f68b618b0e9f1f4e6f01a2d5424de
Mari Sevarolli <marianasevarollisenai@gmail.com>
2024-08-30 11:31:40 -0300

    Espaçamentos necessários colocados nos arquivos

--- testes-e2e-cypress-main/cypress/e2e/formularioCadastro.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/formularioCadastro.cy.js
@@ -2,6 +2,7 @@ describe('Formulário cadastro', () => {
     beforeEach(()=> {
     cy.visit('http://localhost:3000')
     })
+    
     it('Usuário deve conseguir se cadastrar com sucesso', () => {
     cy.getByData('botao-cadastro').click() /* testaremos agora o botão 'cadastrar' */
     cy.getByData('nome-input').type('julio')
--- testes-e2e-cypress-main/cypress/e2e/inicio.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/inicio.cy.js
@@ -2,6 +2,7 @@ describe('Página Principal', () => {
   beforeEach( () => {
     cy.visit('http://localhost:3000/')
   })
+  
   it('Deve renderizar h1 com o texto correto!', () => {
     cy.visit('http://localhost:3000')
     cy.getByData('titulo-principal').contains('Experimente mais liberdade no controle da sua vida financeira.Crie sua conta com a gente!')
